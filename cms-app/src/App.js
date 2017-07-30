dc14ec12f154a8d90177c06d6caa0f6c30a7c372
Mike Nolan <me@michael-nolan.com>
2017-07-30 16:33:32 -0400

    basic scaffolding of cms structure

--- cms-app/src/App.js
+++ cms-app/src/App.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import css from './App.css'
 
 class App extends Component {
-  componentDidMount () {}
+  constructor () {
+    super();
+    this.state = {
+        timemaps: [],
+        selectedMap: null
+    };
+  }
+
+  componentDidMount () {
+    fetch('/api/v1/timemap/1/').then(response => {
+      response.json().then(json => {
+          this.setState({timemaps: [json]})
+      });
+    });
+  }
 
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
+      <div id='footer-wrapper' className='footerWrapper'>
+        <header className='style1'>
           <h2>TimeMapper CMS</h2>
+        </header>
+        <div className='row'>
+            <div className='3u'>
+                hello
+            </div>
+            <div className='9u'>
+                world
+            </div>
         </div>
-        hello
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
